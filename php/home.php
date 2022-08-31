b82d79b89df386f31650ac6fff03f6cb507b34f7
taylawantsasnack <tayla.orsmond@gmail.com>
2022-08-31 08:58:05 +0200

    Created events page and modified profile

--- php/home.php
+++ php/home.php
@@ -25,13 +25,11 @@
         ?>
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h1>home.</h1>
-                </div>
+                <div class="col-12"><h1>home.</h1></div><!--Title-->
             </div>
             <div class="row">
                 <div class="col-12">
-                    <nav class="d-flex justify-content-between align-items-center px-3">
+                    <nav class="d-flex justify-content-between align-items-center px-3"><!--Nav-->
                         <ul class="nav nav-tabs">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#"><span class="h5">your folios.</span></a>
@@ -40,17 +38,17 @@
                                 <a class="nav-link" aria-current="page" href="#"><span class="h5">global folios.</span></a>
                             </li>
                         </ul>
-                        <form>
+                        <form><!--Search-->
                             <div class="input-group">
                                 <input type="text" class="form-control" id="search" placeholder="search">
                                 <button type="submit" class="btn btn-outline-secondary"><i class="fa fa-search"></i></button>
                             </div>
-                        </form>
-                    </nav>
+                        </form><!--End Search-->
+                    </nav><!--End Nav-->
                 </div>
             </div>
-            <div class="row p-3 border" id="event-area">
-                <div class="col-4">
+            <div class="row p-3 border" id="event-area"><!--EVENT AREA-->
+                <div class="col-4"><!--Primary event (featured)-->
                     <div class="card text-bg-dark" id="event-primary">
                         <img src="https://images.unsplash.com/photo-1549887534-1541e9326642?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" class="card-img" alt="...">
                         <div class="card-img-overlay d-flex flex-column justify-content-between">
@@ -64,12 +62,13 @@
                                     <a href="." class="btn btn-dark">#festival</a>
                                     <a href="." class="btn btn-dark">#live</a>
                                 </div>
+                                <a href="event.php" class="stretched-link"></a>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-8 event-area-outer">
-                    <div class="align-items-center event-area-inner">
+                </div><!--End Primary event-->
+                <div class="col-8 event-area-outer"><!--EVENT AREA OUTER (holder for event rows + scrollable container)-->
+                    <div id="ea-1" class="align-items-center event-area-inner"><!--EVENT AREA INNER 1 (where the 1st row of events are loaded-->
                         <div class="card event-card">
                             <img src="https://images.unsplash.com/photo-1456086272160-b28b0645b729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" class="card-img-top img-fluid" alt="...">
                             <div class="card-body">
@@ -119,8 +118,8 @@
                                 <p class="card-text">Pirate's Sports Club</p>
                             </div>
                         </div>
-                    </div>
-                    <div class="align-items-center event-area-inner">
+                    </div><!--END EA INNER 1-->
+                    <div id="ea-2" class="align-items-center event-area-inner"><!--EVENT AREA INNER 2 (where the 1st row of events are loaded-->
                         <div class="card event-card">
                             <img src="https://images.unsplash.com/photo-1456086272160-b28b0645b729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" class="card-img-top img-fluid" alt="...">
                             <div class="card-body">
@@ -170,9 +169,9 @@
                                 <p class="card-text">Pirate's Sports Club</p>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </div><!--END EA INNER 2-->
+                </div><!--END EA OUTER-->
+            </div><!--END EA-->
         </div>
         <?php 
             require_once 'components/footer.php';

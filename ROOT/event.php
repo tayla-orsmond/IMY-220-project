99724fa96be3b342567b1ab3c3689e7fb67c52a4
taylawantsasnack <tayla.orsmond@gmail.com>
2022-10-12 10:39:07 +0200

    Added Favicon

--- ROOT/event.php
+++ ROOT/event.php
@@ -5,6 +5,8 @@
         <meta name="author" content="Tayla Orsmond">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--favicon-->
+        <link rel="icon" href="media/favicon.png" type="image/png" sizes="32x32">
         <title>artfolio | Event</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/8ab8fd8eb6.js" crossorigin="anonymous"></script>

6d08d9706bbc08bd061e4a418a38e754ad7d7d36
taylawantsasnack <tayla.orsmond@gmail.com>
2022-11-01 09:17:15 +0200

    Fixed scrolling issue

--- ROOT/js/scroll.js
+++ ROOT/js/scroll.js
@@ -3,8 +3,11 @@
 // Path: js\scroll.js
 // Description: This is the functionality for the horizontal scrolling divs.
 $(() => {
-    $('.scroller').on('mousewheel', function(event) {
-        $(this).scrollLeft(event.originalEvent.deltaY * 8 + $(this).scrollLeft());
-        event.preventDefault();
+    $('.scroller').on('mousewheel', function (event) {
+        //check if the container is overflowing
+        if (this.scrollWidth > this.clientWidth) {
+            $(this).scrollLeft(event.originalEvent.deltaY * 8 + $(this).scrollLeft());
+            event.preventDefault();
+        }
     });
 });

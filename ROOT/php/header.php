e863c167d5acda6ae384e65ff81ed5b32029b46e
taylawantsasnack <tayla.orsmond@gmail.com>
2022-11-02 11:24:42 +0200

    Minor style changes + responsiveness

--- ROOT/php/header.php
+++ ROOT/php/header.php
@@ -9,7 +9,7 @@ require_once 'class/config.php';
         if user is not logged in - show sign up and log in page
      -->
 <header>
-    <nav class="navbar navbar-expand-lg p-4 w-100">
+    <nav class="navbar navbar-expand-lg p-4 m-0 w-100">
         <div class="container-fluid">
             <a class="navbar-brand h1 ms-2" href="index.php">artfolio.</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,31 +29,31 @@ require_once 'class/config.php';
                     $logged_in = true;
                 }
                 if ($logged_in) {
-                    echo '<ul class="navbar-nav me-2 mb-2 mb-lg-0 align-items-center">
-                                <li class="nav-item mx-2 text-secondary">
+                    echo '<ul class="navbar-nav me-2 mb-2 mb-lg-0">
+                                <li class="nav-item text-center mx-2 text-secondary">
                                     Welcome @' . $_SESSION['user_name'] . '
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item text-center mx-2">
                                     <a class="nav-link" aria-current="page" href="home.php">home.</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item text-center mx-2">
                                     <a class="nav-link" aria-current="page" href="profile.php?id=' . $_SESSION['user_id'] . '"> myfolio.</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item text-center mx-2">
                                     <a class="nav-link btn btn-secondary" href="logout.php">Log out</a>
                                 </li>
                                 ' .
-                        ($_SESSION['user_admin'] == 1 ? '<li class="nav-item mx-2">
+                        ($_SESSION['user_admin'] == 1 ? '<li class="nav-item text-center mx-2">
                                     <a class="nav-link btn btn-secondary" href="admin.php">Admin Zone</a>
                                 </li>' : '')
                         . '
                             </ul>';
                 } else {
                     echo '<ul class="navbar-nav me-2 mb-2 mb-lg-0">
-                                <li class="nav-item mx-2">
+                                <li class="nav-item text-center mx-2">
                                     <a class="nav-link" aria-current="page" href="signup.php">Sign up</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item text-center mx-2">
                                     <a class="nav-link btn btn-secondary" href="login.php">Log in</a>
                                 </li>
                             </ul>';
--- ROOT/profile.php
+++ ROOT/profile.php
@@ -108,7 +108,7 @@
                 echo '
                 <div class="row">
                     <div class="col-12"><h1>' . ($_SESSION['user_id'] == $user_id ? "my" : "art") . 'folio.</h1></div><!--Title-->
-                    <div class="col-9 p-3 p-lg-5 d-flex flex-wrap border"><!--Profile Header card -> show details of the user-->
+                    <div class="col-12 col-lg-9 p-3 p-lg-5 d-flex flex-wrap border"><!--Profile Header card -> show details of the user-->
                         <div class="flex-1 profile-photo">
                             <img src="media/uploads/profiles/' . $profile['u_profile'] . '" alt="..." class="img-fluid rounded-circle">
                         </div>
@@ -130,9 +130,9 @@
                             <p><i class="fa-regular fa-envelope"></i> <a href="mailto:' . $profile['u_email'] . '">' . $profile['u_email'] . '</a></p>
                         </div>
                     </div><!--End Profile Header card-->
-                    <div class="col-3 d-flex flex-column align-items-start justify-content-start gap-4 gap-lg-3 p-1 p-s-2 my-5 p-lg-2 my-lg-0"><!--Actions (follow, DM, edit etc.) -->';
+                    <div class="col-12 col-lg-3 d-flex flex-row flex-lg-column align-items-start justify-content-end justify-content-lg-start gap-4 gap-lg-3 p-1 p-lg-2"><!--Actions (follow, DM, edit etc.) -->';
                 if ((isset($_SESSION['user_id']) && $user_id == $_SESSION['user_id']) || $_SESSION['user_admin'] == 1) {
-                    echo '<a href="" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#edit_profile_modal"><i class="fa-solid fa-palette fa-xl"></i> <span class="d-lg-none">Edit Profile</span></a>';
+                    echo '<a href="" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#edit_profile_modal"><i class="fa-solid fa-palette fa-xl"></i> <span class="d-none d-md-inline">Edit Profile</span></a>';
                 }
                 if (isset($_SESSION['user_id']) && $user_id == $_SESSION['user_id']) {
                     echo '<a href="messages.php?chat=' . $_SESSION['user_id'] . '&chatn=' . $profile['u_name'] . '"><i class="fas fa-inbox fa-xl"></i></a>';
